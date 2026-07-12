a9baa341c9ee51ff2a756b37f3dc67429c30ffd3
ChineduObi <62855420+ChineduObi@users.noreply.github.com>
2026-07-12 16:58:02 -0230

    Add: minimum-stack - submission-3

---
+++ Data Structures & Algorithms/minimum-stack/submission-3.py
@@ -0,0 +1,26 @@
+class MinStack:
+
+    def __init__(self):
+        self.data = []
+        self.minEl = []
+
+    def push(self, val: int) -> None:
+        self.data.append(val)
+
+        if not self.minEl:
+            self.minEl.append(val)
+            
+        else:
+            self.minEl.append(min(val, self.minEl[-1]))
+
+    def pop(self) -> None:
+        self.data.pop()
+        self.minEl.pop()
+        
+
+    def top(self) -> int:
+        return self.data[-1]
+        
+
+    def getMin(self) -> int:
+        return self.minEl[-1]
