292a2cd7ffba5e641c11514c0b48502707b94462
ChineduObi <62855420+ChineduObi@users.noreply.github.com>
2026-08-24 23:15:28 -0230

    Add: trapping-rain-water - submission-3

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-3.py
@@ -0,0 +1,19 @@
+#this is an optimazed solution that just uses two pointers, when the left pointer has a smaller value I will go move right and update the leftMax and the other way for right pointer. BOth leftMax and right Max are initilized with the index of the left and right pointers.
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        if not height:
+            return 0
+
+        l, r = 0, len(height) - 1
+        leftMax, rightMax = height[l], height[r]
+        res = 0
+        while l < r:
+            if leftMax < rightMax:
+                l += 1
+                leftMax = max(leftMax, height[l])
+                res += leftMax - height[l]
+            else:
+                r -= 1
+                rightMax = max(rightMax, height[r])
+                res += rightMax - height[r]
+        return res
